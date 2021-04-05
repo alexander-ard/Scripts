c5968dbbceea7709268ae821001f507e87b46b18
Alexander Ardmirola <alexanderardmirola@gmail.com>
2021-04-04 20:38:14 -0500

    Creted script for creating Vue component files

---
+++ create-vue.sh
@@ -0,0 +1,31 @@
+
+echo "Creating component $1 in $2"
+
+length=${#1}
+
+first=$(echo "${1}" | awk '{ print substr( $0, 0, 1 ) }' | awk '{ print tolower($0); }' )
+
+second=$(echo "${1}" | awk '{ print substr( $0, 2, '"${length}"' ) }')
+
+className="${first}${second}"
+
+cd $2/src/components
+
+mkdir $1
+
+cd $1
+
+echo "<template>
+  <div class="\"${className}\""></div>
+</template>
+
+<script>
+import './${1}.scss';
+export default {};
+</script>" >> ${1}.vue
+
+echo "."${className}" {
+}" >> ${1}.scss
+
+
+echo 'Finished!'
